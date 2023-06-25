b74ef04b5c9d673b1ac55c14b2c1bcbb1dd2e02b
stiva98 <samustiva9810@gmail.com>
2023-06-25 17:09:47 +0200

    ADD css in html

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,29 @@
 const casualNumber = 5;
 const arrCasualNumber = [];
 const maxNumberInArr = 10;
-while (arrCasualNumber.length < casualNumber){
-    const newNumberCasual = randomNumber(1, maxNumberInArr);
-    if (arrCasualNumber.includes(newNumberCasual) == false) {
-        arrCasualNumber.push(newNumberCasual);
-        console.log(newNumberCasual);
+const myContainer = document.querySelector('.container');
+const buttonStart = document.getElementById('start');
+const reset = document.getElementById('reset');
+
+buttonStart.addEventListener('click', function(){
+    while (arrCasualNumber.length < casualNumber){
+        const newNumberCasual = randomNumber(1, maxNumberInArr);
+        if (arrCasualNumber.includes(newNumberCasual) == false) {
+            arrCasualNumber.push(newNumberCasual);
+            console.log(newNumberCasual);
+            const newBox = document.createElement('div');
+            newBox.classList.add('cell');
+            newBox.append(newNumberCasual);
+            myContainer.append(newBox)
+        }
     }
-}
+})
+
+
+reset.addEventListener('click', function (){
+    location.reload();
+
+})
 
 
 
