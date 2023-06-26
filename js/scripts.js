f94b29d6a3dd248516b087f0cdc9836a555cc931
stiva98 <samustiva9810@gmail.com>
2023-06-26 09:50:40 +0200

    EDIT tempo del setTimeout come richiesto

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,8 @@ const maxNumberInArr = 10;
 const myContainer = document.querySelector('.container');
 const buttonStart = document.getElementById('start');
 const reset = document.getElementById('reset');
-
-setTimeout(
-    myTimer, 
-    5000);
+let arrPrompt = [];
+const total = document.querySelector('.score-now');
 
 buttonStart.addEventListener('click', function(){
     while (arrCasualNumber.length < casualNumber){
@@ -22,6 +20,12 @@ buttonStart.addEventListener('click', function(){
             myContainer.append(newBox)
         }
     }
+    setTimeout(
+    myTimer, 
+    30000);
+    setTimeout(
+    timeOut, 
+    30100);
 })
 
 
@@ -58,4 +62,34 @@ function myTimer() {
     for(let i = 0; i < newBoxNone.length; i++){
         newBoxNone[i].classList.add('cell-2');
     }
+}
+function timeOut() {
+    const answers1 = prompt('inserisci il primo numero');
+    const answers2 = prompt('inserisci il secondo numero');
+    const answers3 = prompt('inserisci il terzo numero');
+    const answers4 = prompt('inserisci il quarto numero');
+    const answers5 = prompt('inserisci il quinto numero');
+    
+    const promptBox = document.querySelectorAll('.cell-prompt');
+    arrPrompt = [answers1, answers2, answers3, answers4, answers5]
+    for(let i = 0; i < promptBox.length; i++){
+        const element = promptBox[i];
+        for(let j = 0; j < arrPrompt.length; j++){
+            element.innerHTML = arrPrompt[i];
+        }
+    }
+    const newBoxNone = document.querySelectorAll('.cell');
+    for(let i = 0; i < newBoxNone.length; i++){
+        newBoxNone[i].classList.remove('cell-2');
+    }
+    score ();
+}
+function score () {
+    let result = 0
+    for(let i = 0; i < arrCasualNumber.length; i++){
+        if(arrCasualNumber[i] == arrPrompt[i]){
+            result++;
+        }
+    }
+    total.innerHTML = 'Il tuo score: ' + result;
 }
